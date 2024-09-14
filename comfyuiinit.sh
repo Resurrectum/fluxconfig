8fd3929b4c2784e1a3a510556e7d7505abe9390d
Rafael Kluender <rafael@kluender.ch>
2024-09-14 21:45:04 +0200

    added flux schnell to downloaded models on startup

--- comfyuiinit.sh
+++ comfyuiinit.sh
@@ -27,6 +27,7 @@ NODES=(
 CHECKPOINT_MODELS=(
     # commented out in first attempt because already downloaded below
     # "https://huggingface.co/black-forest-labs/FLUX.1-dev/blob/main/flux1-dev.safetensors"
+    "https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/flux1-schnell.safetensors"
 )
 
 CLIP_MODELS=(
